85b43872b04bd274ceff468e2b96925be6acb248
Prasoon Sharma <prasoon.sharma@systematixindia.com>
2016-07-12 12:37:53 +0530

    test

--- .scripts/install_dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-yum groupinstall -y "PHP Support"  
-yum install -y php-mysql  
-yum install -y nginx  
-yum install -y php-fpm  
--- .scripts/stop_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-isExistApp=`pgrep nginx`  
-if [[ -n  \$isExistApp ]]; then  
-   service nginx stop
-fi  
-isExistApp=`pgrep php-fpm`  
-if [[ -n  \$isExistApp ]]; then  
-    service php-fpm stop
-fi  
